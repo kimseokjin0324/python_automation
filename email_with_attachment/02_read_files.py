33e5c320c2eef2c86f068a6e143f2bf5e912b1bf
kimseokjin0324 <kimseokjin0324@gmail.com>
2023-04-15 03:17:02 +0900

    첨부파일 포함 목록 만들어서 한번에 전송하기 -엑셀을 읽어와서 업체명을 이용해 이메일,담당자명,참조이메일 추출하기

--- email_with_attachment/02_read_files.py
+++ email_with_attachment/02_read_files.py
@@ -1,12 +1,25 @@
 from os import listdir
-def make_email_list(data_path):
+import pandas as pd
+pd.set_option('display.max_columns',None)
+
+
+def make_email_list(data_path,partners_filename):
     filenames = listdir(data_path)
 
+    df_partners = pd.read_excel(partners_filename)
+
+
+    #업체명 추출
     for filename in filenames:
         filename = filename.replace('.xlsx','')
         partner_name = filename.replace('[패스트몰]','')
         print(partner_name)
-
+        found_row = df_partners[df_partners['업체명'].str.contains(partner_name)]
+        email1 = str(found_row['이메일1'].values[0])
+        partner_manager_name = str(found_row['컨택담당자'].values[0])
+        email_cc = str(found_row['참조이메일'].values[0])
+        # print(found_row)
+        print(email1,partner_manager_name,email_cc)               # 이메일1  컨텍담당자 참조이메일
 
 if __name__ == '__main__':
-  make_email_list('data/')
+  make_email_list('data/','파트너목록.xlsx')
